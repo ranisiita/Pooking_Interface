8344f35e9abe6c3b3bd4611c095896fc9f3b79ac
Zapiony <sazapatac4@gmail.com>
2026-04-27 20:53:07 -0500

    feat(navbar): Inclusión de ruta de perfíl

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,11 +3,13 @@ import { HomeComponent } from './pages/home/home.component';
 import { SearchComponent } from './pages/search/search.component';
 import { Login } from './pages/login/login';
 import { Signup } from './pages/signup/signup';
+import { ProfileComponent } from './pages/profile/profile.component';
 
 export const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'buscar', component: SearchComponent },
   { path: 'login', component: Login },
   { path: 'signup', component: Signup },
+  { path: 'profile', component: ProfileComponent },
   { path: '**', redirectTo: '' }
 ];
